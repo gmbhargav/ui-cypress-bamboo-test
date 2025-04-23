dbf6c262b384501910faf54060bd0b4f50b4c0b9
gmbhargav <bhargavagmummaneedi@gmail.com>
2025-04-23 19:22:46 +0800

    Adding first test

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,35 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://magento.softwaretestingboard.com',
+    specPattern: 'cypress/e2e/tests/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.js',
+    fixturesFolder: 'cypress/fixtures',
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
+    downloadsFolder: 'cypress/downloads',
+    video: true,
+    videoCompression: 32,
+    trashAssetsBeforeRuns: true,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
+    requestTimeout: 5000,
+    responseTimeout: 30000,
+    retries: {
+      runMode: 0,
+      openMode: 0,
+    },
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: true,
+      timestamp: 'mmddyyyy_HHMMss',
+      reportTitle: 'Magento Test Report',
+      charts: true,
+      code: true
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
